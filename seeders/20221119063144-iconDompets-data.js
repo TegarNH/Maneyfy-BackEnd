5ef51c9b6846c3cc1a41dcc712f32a6db30f31e1
nee18 <99226926+nee18@users.noreply.github.com>
2022-11-21 22:10:46 +0700

    seed iconD

--- seeders/20221119063144-iconDompets-data.js
+++ seeders/20221119063144-iconDompets-data.js
@@ -1,5 +1,5 @@
 'use strict';
-const IconDompetsData = require('../masterdata/IconDompet.json');
+const IconDompetsData = require('../masterdata/iconDompet.json');
 
 module.exports = {
   async up (queryInterface, Sequelize) {
