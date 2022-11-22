09a0a2a7f10c42b2bd73f5e3f7b65070610b451c
nee18 <99226926+nee18@users.noreply.github.com>
2022-11-22 21:24:28 +0700

    routers

--- routers/index.route.js
+++ routers/index.route.js
@@ -1,11 +1,17 @@
-const express               = require('express');
-const router                = express.Router();
-const userRoutes            = require('./user.route');
-const dompetRoutes          = require('./dompet.route');
-const icondompetRoutes      = require('./iconDompet.route');
-const spendingRoutes        = require('./spending.route');
-const earningRoutes        = require('./earning.route');
-const authController        = require('../controllers/auth.controller');
+const express                = require('express');
+const router                 = express.Router();
+const authController         = require('../controllers/auth.controller');
+const userRoutes             = require('./user.route');
+const dompetRoutes           = require('./dompet.route');
+const iconDompetRoutes       = require('./iconDompet.route');
+
+const spendingRoutes         = require('./spending.route');
+const categorySpendingRoutes = require('./categorySpending.route');
+const iconSpendingRoutes     = require('./iconSpending.route');
+
+const earningRoutes          = require('./earning.route');
+const categoryEarningRoutes  = require('./categoryEarning.route');
+const iconEarningRoutes      = require('./iconEarning.route');
 const { createValidationFor, checkValidationResult } = require('../misc/validator');
 
 
@@ -14,8 +20,14 @@ router.post('/register', createValidationFor('register'), checkValidationResult,
 router.post('/login', createValidationFor("login"), checkValidationResult, authController.login);
 router.use('/user', userRoutes);
 router.use('/dompet', dompetRoutes);
-router.use('/iconDompet', icondompetRoutes);
+router.use('/iconDompet', iconDompetRoutes);
+
 router.use('/spending', spendingRoutes);
+router.use('/categorySpending', categorySpendingRoutes);
+router.use('/iconSpending', iconSpendingRoutes);
+
 router.use('/earning', earningRoutes);
+router.use('/categoryEarning', categoryEarningRoutes);
+router.use('/iconEarning', iconEarningRoutes);
 
 module.exports = router;
