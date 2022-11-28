ea962787053afa49ac3f4d2b90c145abcffe2b51
nee18 <99226926+nee18@users.noreply.github.com>
2022-11-28 20:35:56 +0700

    testing register

---
+++ tests/auth.controller.test.js
@@ -0,0 +1,106 @@
+const authTest = () => {
+    const request = require('supertest')
+    const app = require('../app')
+    jest.setTimeout(30000);
+
+    describe('Register API', () => {
+        it('should register users', async () => {
+            const res = await request(app)
+                .post('/api/register')
+                .set('Content-Type', 'application/json')
+                .send({
+                    name: 'test',
+                    email: 'test@mail.com',
+                    password: '12345678'
+                })
+            expect(res.statusCode).toEqual(201);
+            expect(res.body).toHaveProperty('data');
+        });
+
+        it('throw register error name is null', async () => {
+            const res = await request(app)
+                .post('/api/register')
+                .set('Content-Type', 'application/json')
+                .send({
+                    email: 'test@mail.com',
+                    password: '12345678'
+                })
+            expect(res.statusCode).toEqual(422);
+            expect(res.body).toBeDefined();
+            expect(res.body.errors[0].msg).toEqual("name cant be null");
+        });
+
+        it('throw register error name cant less than 3 characters', async () => {
+            const res = await request(app)
+                .post('/api/register')
+                .set('Content-Type', 'application/json')
+                .send({
+                    name: "te",
+                    email: 'test@mail.com',
+                    password: '12345678'
+                })
+            expect(res.statusCode).toEqual(422);
+            expect(res.body).toBeDefined();
+            expect(res.body.errors[0].msg).toEqual("name must be minimum 3 length");
+        });
+
+        it('throw register error email is null', async () => {
+            const res = await request(app)
+                .post('/api/register')
+                .set('Content-Type', 'application/json')
+                .send({
+                    name: 'test',
+                    password: '12345678'
+                })
+            expect(res.statusCode).toEqual(422);
+            expect(res.body).toBeDefined();
+            expect(res.body.errors[0].msg).toEqual("email cant be null");
+        });
+
+        it('throw register error email must be an email format', async () => {
+            const res = await request(app)
+                .post('/api/register')
+                .set('Content-Type', 'application/json')
+                .send({
+                    name: 'test',
+                    email: 'testmail.com',
+                    password: '12345678'
+                })
+            expect(res.statusCode).toEqual(422);
+            expect(res.body).toBeDefined();
+            expect(res.body.errors[0].msg).toEqual("must be an email");
+        });
+
+        it('throw register error password is null', async () => {
+            const res = await request(app)
+                .post('/api/register')
+                .set('Content-Type', 'application/json')
+                .send({
+                    name: 'test',
+                    email: 'test@mail.com'
+                })
+            expect(res.statusCode).toEqual(422);
+            expect(res.body).toBeDefined();
+            expect(res.body.errors[0].msg).toEqual("password cant be null");
+        });
+
+        it('throw register error password cant less than 8 characters', async () => {
+            const res = await request(app)
+                .post('/api/register')
+                .set('Content-Type', 'application/json')
+                .send({
+                    name: "test",
+                    email: 'test@mail.com',
+                    password: '1234'
+                })
+            expect(res.statusCode).toEqual(422);
+            expect(res.body).toBeDefined();
+            expect(res.body.errors[0].msg).toEqual("password must be minimum 8 length");
+        });
+    })
+
+}
+
+
+
+module.exports = authTest;
---
+++ tests/controller.spec.js
@@ -0,0 +1,12 @@
+const { sequelize } = require("../models");
+
+afterAll(async () => {
+    await sequelize.close();
+});
+
+const authTest = require('./auth.controller.test');
+
+describe('run test sequentially', () => {
+    authTest();
+})
+
