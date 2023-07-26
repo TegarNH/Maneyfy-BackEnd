c5bd0f859a81a98dbe669c4eaa12d7182184f1a9
TegarNH <tegarnaufalh@gmail.com>
2023-07-26 22:55:07 +0700

    update cors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const logger = require('morgan');
 const cors = require('cors');
 
-app.use(cors({ origin: ["http://localhost:8000", "https://maneyfy.vercel.app", "https://maneyfy-backend-production.up.railway.app/api/"] }));
+app.use(cors({ origin: ["http://localhost:8000", "https://v2-maneyfy.vercel.app/", "https://v2-maneyfy-backend.onrender.com/api/"] }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
